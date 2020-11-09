4090d2904921279ab54f6fbb7fbcffffe3d57f1c
smokeytube <67290867+smokeytube@users.noreply.github.com>
2020-11-08 22:27:33 -0500

    br

--- forsyth_board/brandom/views.py
+++ forsyth_board/brandom/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Xeno',
+        'title': 'School lunch',
+        'content': 'bruh',
+        'date_posted': 'August 27, 2018'
+    },
+
+    {
+        'author': 'John roblox',
+        'title': 'bruh',
+        'content': 'i hate school',
+        'date_posted': 'August 28, 2018'
+
+    }
+]
+
+
+
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'brandom/home.html', context)
