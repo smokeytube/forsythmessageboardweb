aa6788728bd9f6c4fb46c4d372faf79c686cd02a
smokeytube <67290867+smokeytube@users.noreply.github.com>
2020-11-14 17:15:17 -0500

    what

--- forsyth_board/forsyth_board/urls.py
+++ forsyth_board/forsyth_board/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -25,3 +27,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('brandom/', include('brandom.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
