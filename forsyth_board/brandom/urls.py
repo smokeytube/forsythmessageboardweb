1121fec3af63add2dbdb167e181a29ceaa7758ee
smokeytube <67290867+smokeytube@users.noreply.github.com>
2020-11-15 21:10:52 -0500

    ayy lesson 10 complete

--- forsyth_board/brandom/urls.py
+++ forsyth_board/brandom/urls.py
@@ -1,11 +1,12 @@
 #from forsyth_board.brandom.models import Post
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 #from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='brandom-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
--- forsyth_board/brandom/views.py
+++ forsyth_board/brandom/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -41,5 +41,16 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         if self.request.user == post.author:
             return True
         return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/brandom/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
         
