3f9f525fa8f1392d70c2658b13b20232db67a89f
smokeytube <67290867+smokeytube@users.noreply.github.com>
2020-11-16 20:05:46 -0500

    lesson 11 done

--- forsyth_board/brandom/urls.py
+++ forsyth_board/brandom/urls.py
@@ -1,10 +1,11 @@
 #from forsyth_board.brandom.models import Post
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 #from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='brandom-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
--- forsyth_board/brandom/views.py
+++ forsyth_board/brandom/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.models import User
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -15,6 +16,17 @@ class PostListView(ListView):
     template_name = 'brandom/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 10
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'brandom/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
