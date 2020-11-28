3a82ad4736f2438c61b0b4d3d1e539859cca3855
smokeytube <67290867+smokeytube@users.noreply.github.com>
2020-11-28 14:23:37 -0500

    about to start 12

--- forsyth_board/forsyth_board/settings.py
+++ forsyth_board/forsyth_board/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '9qxmt5h24*%1q90_d4_mq1ba)mq6wv=bu4fyztyxf(qw4buza$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+#ALLOWED_HOSTS = []
 
 # Application definition
 
